0eb7f82834dae1b68d034d83bb201da722795a32
Charanjit Singh <ckhabra@gmail.com>
2017-08-30 18:46:48 +0530

    fix: minor fix
    Leftover in the state for dx

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 const initialState = {
   activeTab: 'databases', // load-balancers, caches, message-queues, databases, others
   selectedApp: '',
-  isShowingAppModal: true,
+  isShowingAppModal: false,
   selectedAppConfig: {
     version: 'v9.5.1'
   }
